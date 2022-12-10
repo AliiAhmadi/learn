18271985c8df33c6ff77f0d965dcc26fd141ae2e
AliiAhmadi <aliahmadi.github@gmail.com>
2022-12-10 22:47:39 +0330

    add strings.php

---
+++ php/strings.php
@@ -0,0 +1,29 @@
+<?php
+
+// working with strings in php
+
+### is_string() function return an boolean value 
+
+var_dump(is_string(1));
+
+### strlen() function return length of the string
+
+var_dump(strlen("ALiAhmadi"));
+
+### lcfirst() function will return an string that first character converted to lowercase 
+
+var_dump(lcfirst("ALI"));
+
+var_dump(ucfirst("ali"));
+
+
+### ucwords()
+
+var_dump(ucwords("i am from iran"));
+
+
+### strtoupper() & strtolower()
+
+var_dump(strtolower("ALI"));
+
+?>
