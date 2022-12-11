312f041f10d0afc7b609582ad0b0c81d1971aca4
AliiAhmadi <aliahmadi.github@gmail.com>
2022-12-11 16:40:11 +0330

    add date and time functions

---
+++ php/date-time.php
@@ -0,0 +1,23 @@
+<?php
+// working with date & time in php
+
+### date() function => pass it format of date that you want.
+
+echo date("Y:m:d");
+
+### date_default_timezone_set() function
+
+### time() => return timestamp
+
+$now = time();
+
+### mktime() => pass it values and get timestamp of that time
+
+### strtotime() function -> pass it date in string format and get timestamp of that date.
+
+### microtime() function
+
+# END
+
+
+?>
