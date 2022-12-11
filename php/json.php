9f02821e892b6e010195e480aff6e33063fd5fda
AliiAhmadi <aliahmadi.github@gmail.com>
2022-12-11 19:10:26 +0330

    add json functions

--- php/json.php
+++ php/json.php
@@ -1,4 +1,19 @@
 <?php
+// working with json in php
+
+### json_decode() & json_encode()
+//        convert from json to array and from array to json
+
+$arr = [
+    "ali",
+    "reza",
+    "mohammad",
+    "zaynab",
+    "yasin"
+];
+
+echo json_encode($arr);
+
 
 
 
