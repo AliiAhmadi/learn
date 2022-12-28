72ffee75d346b33d604a4750f3049d34c52dd807
AliiAhmadi <aliahmadi.github@gmail.com>
2022-12-28 10:28:55 +0330

    utility

---
+++ OOP-database/static/utility.php
@@ -0,0 +1,26 @@
+<?php
+
+class Url
+{
+    public static function current(): string
+    {
+        $url = "";
+
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+            $url .= "https";
+        } else {
+            $url .= "http";
+        }
+        $url .= "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        return $url;
+    }
+
+    public static function segments(string|null $url = null): array
+    {
+        if (!$url) {
+            $url = self::current();
+        }
+        $segments = explode("/", $url);
+        return $segments;
+    }
+}
