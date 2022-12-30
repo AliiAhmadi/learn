9b3a80602ddf51fc5705307a43a12b1348259ec6
AliiAhmadi <aliahmadi.github@gmail.com>
2022-12-30 20:30:05 +0330

    Magic Methods

---
+++ magic/magic.php
@@ -0,0 +1,37 @@
+<?php
+
+class Magic
+{
+    public function __construct()
+    {
+    }
+
+    public function __sleep()
+    {
+    }
+
+    public function __serialize(): array
+    {
+        return [];
+    }
+
+    public function __unserialize(array $data): void
+    {
+    }
+
+    public function __destruct()
+    {
+    }
+
+    public function __wakeup()
+    {
+    }
+
+    public function __toString()
+    {
+    }
+
+    public function __invoke()
+    {
+    }
+}
