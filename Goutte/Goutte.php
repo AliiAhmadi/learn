3c65df3294f2e722fee76ff66b01b228930fb720
AliiAhmadi <aliahmadi.github@gmail.com>
2023-01-02 18:16:39 +0330

    digi-scrapper

--- Goutte/Goutte.php
+++ Goutte/Goutte.php
@@ -2,31 +2,26 @@
 
 require_once "vendor/autoload.php";
 
-$links = [];
-
 use Goutte\Client;
-use Symfony\Component\HttpClient\HttpClient;
+
+$url = "https://www.digikala.com/product/dkp-8169599/%DA%AF%D9%88%D8%B4%DB%8C-%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84-%D8%B4%DB%8C%D8%A7%D8%A6%D9%88%D9%85%DB%8C-%D9%85%D8%AF%D9%84-redmi-note-11-pro-4g-%D8%AF%D9%88-%D8%B3%DB%8C%D9%85-%DA%A9%D8%A7%D8%B1%D8%AA-%D8%B8%D8%B1%D9%81%DB%8C%D8%AA-128-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA-%D9%88-%D8%B1%D9%85-8-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA/";
+
+$data = [];
+
 
 $client = new Client();
 
-$crawler = $client->request("GET", "https://www.google.com/");
+$crawler = $client->request("GET", $url);
 
 
+$data["title"] = $crawler->filter(".color-900");
 
-$crawler->filter('a')->each(function ($node) {
-    // $GLOBALS["links"] = $link;
-    show($node);
-});
 
-// $crawler = $client->request('GET', 'https://www.google.com/');
-// $crawler->filter('a')->each(function ($node) {
-//     print $node->text()."\n";
-// });
 function show($data){
     echo "<pre>";
     print_r($data);
     echo "</pre>";
-    echo "--------------------------------------------------------------------------------------------";
 }
 
-// show($links);
+
+show($data);
